48b89a753d327fbd469f47def54fd483b32aa9b3
Mark Plover <mark@mplover.com>
2025-07-16 01:01:47 -0400

    Upgraded to Tailwind 4

---
+++ src/app/(payload)/template.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '@/styles/payloadStyles.css'
+
+export default function PayloadTemplate({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
